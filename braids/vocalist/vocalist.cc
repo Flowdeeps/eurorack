041bea5e433a71cf489b58a44dbcda963e85c692
Tom Burns <tom@burns.ca>
2018-01-03 02:56:38 +0000

    bring in the vocalist

---
+++ braids/vocalist/vocalist.cc
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "vocalist.h"
+#include "wordlist.h"
+
+void Vocalist::SetBank(unsigned char b) {
+  bank = b;
+}
+
+void Vocalist::SetWord(unsigned char w) {
+  word = w;
+}
+
+void Vocalist::Load() {
+  playing = false;
+  if (mode == MODE_NORMAL) {
+    sam.LoadNextWord(
+      &data[wordpos[bank][word]],
+      &data[wordpos[bank][word] + wordlen[bank][word]],
+      &data[wordpos[bank][word] + (wordlen[bank][word] << 1)],
+      wordlen[bank][word]
+    );
+    sam.InitFrameProcessor();
+    sam.PrepareFrames();
+  } else {
+    LoadRando();
+  }
+}
+
+void Vocalist::LoadRando() {
+  for (int i = 0; i < 24; i++) {
+    rando[i] = rand() % 256;
+  }
+  sam.LoadNextWord(&rando[0], &rando[8], &rando[16], 8);
+  sam.InitFrameProcessor();
+  sam.PrepareFrames();
+}
+
+void Vocalist::FillBuffer(int16_t *output, int len) {
+  if (risingEdge) {
+    Load();
+    risingEdge = false;
+  }
+  if (!playing && gatestate) {
+    playing = true;
+  }
+
+  int written = 0;
+  if (playing) {
+    written = sam.FillBufferFromFrame(len, &output[0]);
+  }
+
+  if (written < len) {
+    if (mode == MODE_NORMAL) {
+      playing = false;
+      SetWord((word + 1) % NUM_WORDS);
+    } else {
+      LoadRando();
+    }
+  }
+
+  for (int i = written; i < len; i++) {
+    output[i] = 0x80;
+  }
+}
+
+void Vocalist::set_gatestate(bool gs) {
+  if (!gatestate && gs) {
+    risingEdge = true;
+  }
+  gatestate = gs;
+}
+
+
---
+++ braids/vocalist/vocalist.h
@@ -0,0 +1,50 @@
+#ifndef __VOCALIST_H__
+#define __VOCALIST_H__
+
+#include "sam/sam.h"
+
+#define MODE_NORMAL 0
+#define MODE_CRAZY 1
+#define MODE_REALLY_CRAZY 2
+
+class Vocalist {
+public:
+  Vocalist() {
+    playing = false;
+    bank = 0;
+    word = 0;
+    risingEdge = 0;
+    mode = MODE_NORMAL;
+    sam.InitFrameProcessor();
+    sam.EnableSingmode();
+  }
+
+  ~Vocalist() { }
+
+  void FillBuffer(int16_t *output, int len);
+  void set_gatestate(bool gs);
+  void SetBank(unsigned char b);
+  void SetWord(unsigned char b);
+
+  void SetMode(int m) { mode = m; }
+  void SetPitch(unsigned char pitch) { sam.SetPitch(pitch); }
+  void SetMouth(unsigned char mouth) { sam.SetMouth(mouth); }
+  void SetThroat(unsigned char throat) { sam.SetThroat(throat); }
+  void SetSpeed(unsigned char speed) { sam.SetSpeed(speed); }
+
+private:
+
+  void Load();
+  void LoadRando();
+
+  SAM sam;
+  int mode;
+  unsigned char bank;
+  unsigned char word;
+  bool gatestate;
+  bool risingEdge;
+  bool playing;
+  unsigned char rando[24];
+};
+
+#endif
